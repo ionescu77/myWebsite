46b88a98fd9d29581911ac72941124913b88ef7b
Razvan Andrei Ionescu <ionescu77@gmail.com>
2015-05-29 02:01:01 +0300

    Added tests for admin auth, fixtures for test

--- src/blogengine/tests.py
+++ src/blogengine/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 
 from django.utils import timezone
 from blogengine.models import Post
@@ -30,3 +30,41 @@ class PostTest(TestCase):
       self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
       self.assertEquals(only_post.pub_date.second, post.pub_date.second)
 
+class AdminTest(LiveServerTestCase):
+    # We need to fill the auth database for login test
+    fixtures = ['users.json']
+
+    def test_login(self):
+        # Create client
+        c = Client()
+        # Get login page
+        response = c.get('/admin/', follow=True)
+        # Check response code Django does redirect on Admin so code 302, we need to set follow=True
+        self.assertEquals(response.status_code, 200)
+        # Check 'Log in' in admin webpage
+        self.assertTrue('Log in' in response.content)
+        # Log the user in
+        c.login(username='testuser', password="test")
+        # Check response code
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+
+    def test_logout(self):
+        # Create client
+        c = Client()
+        # Login
+        c.login(username='testuser', password='test')
+        # Check response code
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+        # Log out
+        c.logout()
+        # Check response code
+        response = c.get('/admin/', follow=True)
+        self.assertEquals(response.status_code, 200)
+        # Check 'Log in' in response
+        self.assertTrue('Log in again' in response.content)
