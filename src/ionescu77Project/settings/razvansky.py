9bcdf6e901afaf50ac91305fa1b14c58bbd7d20c
Razvan Andrei Ionescu <ionescu77@gmail.com>
2015-07-10 16:36:24 +0300

    PostHoliday commit changes like cosmetics for html templates, tests and requirements

--- src/ionescu77Project/settings/razvansky.py
+++ src/ionescu77Project/settings/razvansky.py
@@ -8,5 +8,13 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
+INSTALLED_APPS += (
+    'blogengine',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+)
+
+SITE_ID = 1
+
 #TEST_DATABASE_CHARSET=UTF8
 #CHARSET=UTF8 # supported for PG and MySQL only
